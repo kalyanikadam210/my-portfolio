5d140f7fbc3146131d877dc95768a49435bcfe1d
kalyanikadam210 <kalyanikadam210@gmail.com>
2024-11-07 21:38:41 +0530

    upadate scss

--- src/Contact/index.js
+++ src/Contact/index.js
@@ -1,66 +1,83 @@
 import React from "react";
+import PageHeaderContent from "../../src/utils.js/PageHeaderContent";
+import { BsInfoCircleFill } from "react-icons/bs";
+import { Animate } from "react-simple-animate";
+import "./style.scss";
 
-import PageHeaderContent from "../utils.js/PageHeaderContent";
-import { BsInfoCircleFill } from 'react-icons/bs'
-import { Animate } from 'react-simple-animate'
-import './style.scss';
-
-
-import '../Contact/style.scss'
 const Contact = () => {
   return (
     <section id="contact" className="contact">
-      
-
-      <div className="contact_content">
+      <PageHeaderContent
+        headerText="My Contact"
+        icon={<BsInfoCircleFill size={40} />}
+      />
+      <div className="contact__content">
         <Animate
           play
           duration={1}
           delay={0}
           start={{
-            transform: 'translateX(-200px)'
+            transform: "translateX(-200px)",
           }}
           end={{
-            transform: 'translateX(0px)'
-          }}>
-          
+            transform: "translateX(0px)",
+          }}
+        >
+          <h3 className="contact__content__header-text">Let's Talk</h3>
         </Animate>
         <Animate
           play
           duration={1}
           delay={0}
           start={{
-            transform: 'translateX(200px)'
+            transform: "translateX(200px)",
           }}
           end={{
-            transform: 'translateX(0px)'
-          }}>
-
-          <div className="contact_content_form">
-            <div className="contact_content_form_controlswrapper">
+            transform: "translateX(0px)",
+          }}
+        >
+          <div className="contact__content__form">
+            <div className="contact__content__form__controlswrapper">
               <div>
-                <input required name="name" className="inputName" type={"text"} />
-                <label htmlFor="name" className="nameLabel">Name</label>
+                <input
+                  required
+                  name="name"
+                  className="inputName"
+                  type={"text"}
+                />
+                <label htmlFor="name" className="nameLabel">
+                  Name
+                </label>
               </div>
-              <div >
-                <input required name="email" className="inputEmail" type={"text"} />
-                <label htmlFor="email" className="emailLabel">Email</label>
+              <div>
+                <input
+                  required
+                  name="email"
+                  className="inputEmail"
+                  type={"text"}
+                />
+                <label htmlFor="email" className="emailLabel">
+                  Email
+                </label>
               </div>
-              <div >
-                <textarea required name="description" className="inputdescription" type={"text"} rows="5" />
-                <label htmlFor="description" className="descriptionLabel">Description</label>
+              <div>
+                <textarea
+                  required
+                  name="description"
+                  className="inputDescription"
+                  type={"text"}
+                  rows="5"
+                />
+                <label htmlFor="description" className="descriptionLabel">
+                  Description
+                </label>
               </div>
-
-
             </div>
-            <button>Sumbit</button>
+            <button>Submit</button>
           </div>
         </Animate>
       </div>
     </section>
-  )
-}
+  );
+};
 export default Contact;
-
-
-
--- src/Resume/index.js
+++ src/Resume/index.js
@@ -1,119 +1,83 @@
 import React from "react";
 import PageHeaderContent from "../utils.js/PageHeaderContent";
-import { BsInfoCircleFill } from 'react-icons/bs'
-import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import { BsInfoCircleFill } from "react-icons/bs";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
 
-import 'react-vertical-timeline-component/style.min.css';
+import "react-vertical-timeline-component/style.min.css";
 
-import { data } from './utils';
-import './style.scss';
-import { MdWork } from "react-icons/md"
-
-
-
-
+import { data } from "./utils";
+import "./style.scss";
+import { MdWork } from "react-icons/md";
 
 const Resume = () => {
-   return (
-      <section id="resume" className="resume">
-      
-
-         <div className="timeline">
-
-            <div className="timeline_certificate">
-               <h3 className="timeline_certificate_header-text">Certificate</h3>
-               <VerticalTimeline
-                  layout={'1-column'}
-                  lineColor="var(--yellow-theme-main-color)"
-               >
-                  {data.Certificate.map((item, i) => (
-                     <VerticalTimelineElement
-                        key={i}
-                        className="timeline_certificate_vertical-timeline-element"
-                        contentStyle={
-                           {
-                              background: 'none',
-                              color: "var(--yellow-theme-sub-text-color)",
-                              border: '1.5px solid var(--yellow-theme-main-color)'
-                           }
-                        }
-                        date="2024- present"
-                        icon={<MdWork />}
-                        iconStyle={
-                           {
-                              background: "#181818",
-                              color: "var(--yellow-theme-main-color)",
-
-
-                           }}
-                     >
-                        <div className="vertical-timeline-element-title-wrapper">
-                           <h3>
-                              {item.title}
-                           </h3>
-                           <h4>
-                              {item.subTitle}
-                           </h4>
-                           
-
-                        </div>
-                        <p>{item.description}</p>
-                     </VerticalTimelineElement>
-                  ))
-                  }
-
-
-               </VerticalTimeline>
-
-
-            </div>
-            <div className="timeline_education">
-               <h3 className="timeline_education_header-text">Education</h3>
-               <VerticalTimeline
-                  layout={'1-column'}
-                  lineColor="var(--yellow-theme-main-color)"
-               >
-
-                  {data.education.map((item, i) => (
-                     <VerticalTimelineElement
-                        key={i}
-                        className="timeline_certificate_vertical-timeline-element"
-                        contentStyle={
-                           {
-                              background: 'none',
-                              color: "var(--yellow-theme-sub-text-color)",
-                              border: '1.5px solid var(--yellow-theme-main-color)'
-                           }
-                        }
-                        date="2020 - present"
-                        icon={<MdWork />}
-                        iconStyle={
-                           {
-                              background: "#181818",
-                              color: "var(--yellow-theme-main-color)",
-
-
-                           }}
-                     >
-                        <div className="vertical-timeline-element-title-wrapper">
-                           <h3>
-                              {item.title}
-                           </h3>
-                           <h4>
-                              {item.subTitle}
-                           </h4>
-                          
-
-                        </div>
-                        <p>{item.description}</p>
-                     </VerticalTimelineElement>
-                  ))
-                  }
-               </VerticalTimeline>
-
-            </div>
-         </div>
-      </section>
-   )
-}
+  return (
+    <section id="resume" className="resume">
+      <div className="timeline">
+        <div className="timeline_certificate">
+          <h3 className="timeline_certificate_header-text">Certificate</h3>
+          <VerticalTimeline
+            layout={"1-column"}
+            lineColor="var(--yellow-theme-main-color)"
+          >
+            {data.Certificate.map((item, i) => (
+              <VerticalTimelineElement
+                key={i}
+                className="timeline_certificate_vertical-timeline-element"
+                contentStyle={{
+                  background: "none",
+                  color: "var(--yellow-theme-sub-text-color)",
+                  border: "1.5px solid var(--yellow-theme-main-color)",
+                }}
+                icon={<MdWork />}
+                iconStyle={{
+                  background: "#181818",
+                  color: "var(--yellow-theme-main-color)",
+                }}
+              >
+                <div className="vertical-timeline-element-title-wrapper">
+                  <h3>{item.title}</h3>
+                  <h4>{item.subTitle}</h4>
+                </div>
+                <p>{item.description}</p>
+              </VerticalTimelineElement>
+            ))}
+          </VerticalTimeline>
+        </div>
+        <div className="timeline_education">
+          <h3 className="timeline_education_header-text">Education</h3>
+          <VerticalTimeline
+            layout={"1-column"}
+            lineColor="var(--yellow-theme-main-color)"
+          >
+            {data.education.map((item, i) => (
+              <VerticalTimelineElement
+                key={i}
+                className={`timeline_certificate_vertical-timeline-element  ${item.styleClass}`}
+                contentStyle={{
+                  background: "none",
+                  color: "var(--yellow-theme-sub-text-color)",
+                  border: "1.5px solid var(--yellow-theme-main-color)",
+                }}
+                icon={<MdWork />}
+                iconStyle={{
+                  background: "#181818",
+                  color: "var(--yellow-theme-main-color)",
+                }}
+              >
+                <div className={`vertical-timeline-element-title-wrapper`}>
+                  <h3>{item.title}</h3>
+                  <h4>{item.subTitle}</h4>
+                </div>
+                <p>{item.description}</p>
+              </VerticalTimelineElement>
+            ))}
+          </VerticalTimeline>
+        </div>
+      </div>
+    </section>
+  );
+};
 export default Resume;
